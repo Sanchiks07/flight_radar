736b87489a56b803c57969ffffed99d0185a43c9
Sanchiks07 <ipb23.s.mitniece@vtdt.edu.lv>
2026-01-21 10:59:50 +0200

    globuss

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? "Flight Radar" }}</title>
     <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <script src="//cdn.jsdelivr.net/npm/globe.gl"></script>
 </head>
 <body>
     {{ $slot }}
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,45 +1,108 @@
 <x-layout>
-    <h1>Laravel Google Maps</h1>
-    <div id="map"></div>
+    <div id="globeViz"></div>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAP_KEY') }}&loading=async&callback=initMap" async></script>
+    <script type="module">
+        import { TextureLoader, ShaderMaterial, Vector2 } from 'https://esm.sh/three';
+        import * as solar from 'https://esm.sh/solar-calculator';
 
-    <script>
-        let map, activeInfoWindow, markers = [];
+        const VELOCITY = 1;
 
-        /* ---------- Initialize Map ---------- */
-        function initMap() {
-            map = new google.maps.Map(document.getElementById("map"), {
-                center: {
-                    lat: 56.8801729,
-                    lng: 24.6057484,
-                },
-                zoom: 7
-            });
+        // Custom shader:  Blends night and day images to simulate day/night cycle
+        const dayNightShader = {
+        vertexShader: `
+            varying vec3 vNormal;
+            varying vec2 vUv;
+            void main() {
+                vNormal = normalize(normalMatrix * normal);
+                vUv = uv;
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            }
+        `,
+        fragmentShader: `
+            #define PI 3.141592653589793
+            uniform sampler2D dayTexture;
+            uniform sampler2D nightTexture;
+            uniform vec2 sunPosition;
+            uniform vec2 globeRotation;
+            varying vec3 vNormal;
+            varying vec2 vUv;
 
-            map.addListener("click", function(event) {
-                mapClicked(event);
-            });
-        }
-        
-        /* ---------- Handle Map Click Event ---------- */
-        function mapClicked(event) {
-            console.log(map);
-            console.log(event.latLng.lat(), event.latLng.lng());
-        }
+            float toRad(in float a) {
+                return a * PI / 180.0;
+            }
 
-        /* ---------- Handle Marker Click Event ---------- */
-        function markerClicked(marker, index) {
-            console.log(map);
-            console.log(marker.position.lat());
-            console.log(marker.position.lng());
-        }
+            vec3 Polar2Cartesian(in vec2 c) { // [lng, lat]
+            float theta = toRad(90.0 - c.x);
+            float phi = toRad(90.0 - c.y);
+                return vec3( // x,y,z
+                    sin(phi) * cos(theta),
+                    cos(phi),
+                    sin(phi) * sin(theta)
+                );
+            }
 
-        /* ---------- Handle Marker DragEnd Event ---------- */
-        function markerDragEnd(event, index) {
-            console.log(map);
-            console.log(event.latLng.lat());
-            console.log(event.latLng.lng());
-        }
+            void main() {
+                float invLon = toRad(globeRotation.x);
+                float invLat = -toRad(globeRotation.y);
+                mat3 rotX = mat3(
+                    1, 0, 0,
+                    0, cos(invLat), -sin(invLat),
+                    0, sin(invLat), cos(invLat)
+                );
+                mat3 rotY = mat3(
+                    cos(invLon), 0, sin(invLon),
+                    0, 1, 0,
+                    -sin(invLon), 0, cos(invLon)
+                );
+                vec3 rotatedSunDirection = rotX * rotY * Polar2Cartesian(sunPosition);
+                float intensity = dot(normalize(vNormal), normalize(rotatedSunDirection));
+                vec4 dayColor = texture2D(dayTexture, vUv);
+                vec4 nightColor = texture2D(nightTexture, vUv);
+                float blendFactor = smoothstep(-0.1, 0.1, intensity);
+                gl_FragColor = mix(nightColor, dayColor, blendFactor);
+            }
+        `
+        };
+
+        const sunPosAt = dt => {
+        const day = new Date(+dt).setUTCHours(0, 0, 0, 0);
+        const t = solar.century(dt);
+        const longitude = (day - dt) / 864e5 * 360 - 180;
+        return [longitude - solar.equationOfTime(t) / 4, solar.declination(t)];
+        };
+
+        let dt = +new Date();
+
+        const world = new Globe(document.getElementById('globeViz'));
+
+        Promise.all([
+        new TextureLoader().loadAsync('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-day.jpg'),
+        new TextureLoader().loadAsync('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg')
+        ]).then(([dayTexture, nightTexture]) => {
+        const material = new ShaderMaterial({
+            uniforms: {
+            dayTexture: { value: dayTexture },
+            nightTexture: { value: nightTexture },
+            sunPosition: { value: new Vector2() },
+            globeRotation: { value: new Vector2() }
+            },
+            vertexShader: dayNightShader.vertexShader,
+            fragmentShader: dayNightShader.fragmentShader
+        });
+
+        world.globeMaterial(material)
+            .backgroundImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png')
+            // Update globe rotation on shader
+            .onZoom(({ lng, lat }) => material.uniforms.globeRotation.value.set(lng, lat));
+
+        requestAnimationFrame(() =>
+            (function animate() {
+            // animate time of day
+            dt += VELOCITY * 60 * 24; // the shadow movement (how fast it moves)
+            material.uniforms.sunPosition.value.set(...sunPosAt(dt));
+            requestAnimationFrame(animate);
+            })()
+        );
+        });
     </script>
 </x-layout>
